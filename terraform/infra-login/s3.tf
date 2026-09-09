a6a8d860c1137e22abf8de9cab867dbdd3017fe3
Luiz Felipe <felipediogo09@hotmail.com>
2026-09-09 12:59:57 -0300

    feat: adicionando Server side encryption ao S3

--- terraform/infra-login/s3.tf
+++ terraform/infra-login/s3.tf
@@ -9,3 +9,14 @@ resource "aws_s3_bucket_versioning" "this" {
     status = var.s3_bucket.versioning
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = "secret/aws/s3"
+      sse_algorithm     = "AES256"
+    }
+  }
+}
