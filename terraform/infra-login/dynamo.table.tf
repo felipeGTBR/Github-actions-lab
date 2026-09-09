9c8d300a29588dacd8f5391d9849bae88cbb2e57
Luiz Felipe <felipediogo09@hotmail.com>
2026-09-09 12:57:33 -0300

    adicionando Point in time recovery e Server side encryption

--- terraform/infra-login/dynamo.table.tf
+++ terraform/infra-login/dynamo.table.tf
@@ -2,6 +2,12 @@ resource "aws_dynamodb_table" "this" {
   name         = var.dynamodb_table.name
   billing_mode = var.dynamodb_table.billing_mode
   hash_key     = var.dynamodb_table.hash_key
+  point_in_time_recovery {
+    enabled = var.domain_dynamodb_table.point-in-time-recovery
+  }
+  server_side_encryption {
+    enabled = var.domain_dynamodb_table.server-side-encryption
+  }
 
   attribute {
     name = var.dynamodb_table.hash_key
